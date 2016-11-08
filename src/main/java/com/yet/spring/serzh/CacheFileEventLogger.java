dcd746ff98f2064c3d61791941fce1fb61d44f68
Just-Fun <Just-Fun@users.noreply.github.com>
2016-11-08 22:07:19 +0200

    add write in file

---
+++ src/main/java/com/yet/spring/serzh/CacheFileEventLogger.java
@@ -0,0 +1,10 @@
+package com.yet.spring.serzh;
+
+/**
+ * Created by Serzh on 11/8/16.
+ */
+public class CacheFileEventLogger extends FileEventLogger {
+    public CacheFileEventLogger(String filename) {
+        super(filename);
+    }
+}
